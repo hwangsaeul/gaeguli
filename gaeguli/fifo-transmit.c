d63d45d216fdfac95f183ec04db58fdaad5dce89
J. Kim <jeongseok.kim@sk.com>
2019-09-04 14:56:49 +0900

    fifo-transmit: use GIOChannel to read fifo

--- gaeguli/fifo-transmit.c
+++ gaeguli/fifo-transmit.c
@@ -12,7 +12,12 @@
 #include <gio/gnetworking.h>
 #include <glib/gstdio.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
 #include <srt.h>
 
 /* *INDENT-OFF* */
@@ -23,7 +28,6 @@
    \"mode\": %" G_GINT32_FORMAT " \
 }"
 /* *INDENT-ON* */
-
 typedef struct _SRTInfo
 {
   gint refcount;
@@ -33,6 +37,7 @@ typedef struct _SRTInfo
 
   SRTSOCKET sock;
   gint poll_id;
+
 } SRTInfo;
 
 static SRTInfo *
@@ -76,6 +81,8 @@ srt_info_unref (SRTInfo * info)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (SRTInfo, srt_info_unref)
 /* *INDENT-ON* */
 
+#define BUFSIZE         8192
+
 struct _GaeguliFifoTransmit
 {
   GObject parent;
@@ -83,7 +90,12 @@ struct _GaeguliFifoTransmit
   gchar *fifo_dir;
   gchar *fifo_path;
 
+  GCancellable *cancellable;
+  GIOChannel *io_channel;
+
   GHashTable *sockets;
+
+  gchar buf[BUFSIZE];
 };
 
 /* *INDENT-OFF* */
@@ -123,12 +135,17 @@ gaeguli_fifo_transmit_dispose (GObject * object)
 {
   GaeguliFifoTransmit *self = GAEGULI_FIFO_TRANSMIT (object);
 
+  g_cancellable_cancel (self->cancellable);
+
   g_clear_pointer (&self->sockets, g_hash_table_unref);
+  g_clear_object (&self->io_channel);
 
   _delete_fifo_path (self);
 
   g_clear_pointer (&self->fifo_dir, g_free);
   g_clear_pointer (&self->fifo_path, g_free);
+
+  g_clear_object (&self->cancellable);
 
   G_OBJECT_CLASS (gaeguli_fifo_transmit_parent_class)->dispose (object);
 }
@@ -144,16 +161,41 @@ gaeguli_fifo_transmit_class_init (GaeguliFifoTransmitClass * klass)
 static void
 gaeguli_fifo_transmit_init (GaeguliFifoTransmit * self)
 {
+  self->cancellable = g_cancellable_new ();
   self->sockets = g_hash_table_new_full (g_str_hash, g_str_equal,
       (GDestroyNotify) g_free, (GDestroyNotify) srt_info_unref);
+}
+
+static GaeguliFifoTransmit *
+gaeguli_fifo_transmit_new_full (const gchar * tmpdir)
+{
+
+  g_autoptr (GaeguliFifoTransmit) self = NULL;
+
+  self = g_object_new (GAEGULI_TYPE_FIFO_TRANSMIT, NULL);
+
+  self->fifo_dir = g_strdup (tmpdir);
+  self->fifo_path = g_build_filename (self->fifo_dir, "fifo", NULL);
+
+  if (access (self->fifo_path, F_OK) == -1) {
+    /* TODO: error handling */
+    mkfifo (self->fifo_path, 0666);
+  }
+
+  return g_steal_pointer (&self);
 }
 
 GaeguliFifoTransmit *
 gaeguli_fifo_transmit_new (void)
 {
+  const gchar *sys_tmpdir = g_get_tmp_dir ();
+  g_autofree gchar *tmpdir = NULL;
   g_autoptr (GaeguliFifoTransmit) self = NULL;
 
-  self = g_object_new (GAEGULI_TYPE_FIFO_TRANSMIT, NULL);
+  tmpdir = g_build_filename (sys_tmpdir, "gaeguli-fifo-XXXXXX", NULL);
+  g_mkdtemp (tmpdir);
+
+  self = gaeguli_fifo_transmit_new_full (tmpdir);
 
   return g_steal_pointer (&self);
 }
@@ -163,20 +205,26 @@ gaeguli_fifo_transmit_get_fifo (GaeguliFifoTransmit * self)
 {
   g_return_val_if_fail (GAEGULI_IS_FIFO_TRANSMIT (self), NULL);
 
-  if (self->fifo_dir == NULL) {
-    /* create fifo path */
-    const gchar *tmpdir = g_get_tmp_dir ();
-    self->fifo_dir = g_build_filename (tmpdir, "gaeguli-fifo-XXXXXX", NULL);
-    g_mkdtemp (self->fifo_dir);
-    g_free (self->fifo_path);
-    self->fifo_path = g_build_filename (self->fifo_dir, "fifo", NULL);
-    if (access (self->fifo_path, F_OK) == -1) {
-      /* TODO: error handling */
-      mkfifo (self->fifo_path, 0666);
-    }
-    g_debug ("created fifo (%s)", self->fifo_path);
-  }
   return self->fifo_path;
+}
+
+static gboolean
+_recv_stream (GIOChannel * channel, GIOCondition cond, gpointer user_data)
+{
+  g_autoptr (GError) error = NULL;
+  GaeguliFifoTransmit *self = GAEGULI_FIFO_TRANSMIT (user_data);
+  gsize read_len = 0;
+  GIOStatus io_status;
+
+  g_debug ("(%s):%s%s%s%s", self->fifo_path,
+      (cond & G_IO_ERR) ? " ERR" : "",
+      (cond & G_IO_HUP) ? " HUP" : "",
+      (cond & G_IO_IN) ? " IN" : "", (cond & G_IO_PRI) ? " PRI" : "");
+
+  io_status =
+      g_io_channel_read_chars (channel, self->buf, BUFSIZE, &read_len, &error);
+
+  return TRUE;                  /* should keep continuing */
 }
 
 guint
@@ -208,6 +256,23 @@ gaeguli_fifo_transmit_start (GaeguliFifoTransmit * self,
   }
 
   g_debug ("Created SRT connection (n: %d)", g_hash_table_size (self->sockets));
+
+  if (self->io_channel == NULL) {
+    gint fd = open (self->fifo_path, O_NONBLOCK | O_RDONLY);
+
+    g_debug ("opening io channel (%s)", self->fifo_path);
+
+    /* It's time to read bytes from fifo */
+    self->io_channel = g_io_channel_unix_new (fd);
+    g_io_channel_set_close_on_unref (self->io_channel, TRUE);
+    g_io_channel_set_encoding (self->io_channel, NULL, NULL);
+    g_io_channel_set_buffered (self->io_channel, FALSE);
+    g_io_channel_set_flags (self->io_channel, G_IO_FLAG_NONBLOCK, NULL);
+
+    g_io_add_watch (self->io_channel, G_IO_IN | G_IO_PRI | G_IO_ERR | G_IO_HUP,
+        _recv_stream, self);
+  }
+
 out:
   return transmit_id;
 }
@@ -240,5 +305,15 @@ gaeguli_fifo_transmit_stop (GaeguliFifoTransmit * self,
   }
   g_debug ("Removed SRT connection (n: %d)", g_hash_table_size (self->sockets));
 
+  if (g_hash_table_size (self->sockets) == 0) {
+    g_autoptr (GError) err = NULL;
+    GIOStatus io_status = g_io_channel_shutdown (self->io_channel, TRUE, &err);
+
+    if (io_status != G_IO_STATUS_NORMAL) {
+      g_error ("Failed to close io channel (reason: %s)", err->message);
+    }
+
+    g_clear_object (&self->io_channel);
+  }
   return ret;
 }
