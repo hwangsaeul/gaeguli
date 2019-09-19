d856fc971954bbb5c2bc0d00a3a3152ea4aa2d98
Jakub Adam <jakub.adam@collabora.com>
2019-09-19 15:13:50 +0200

    Add fifo-transmit-add-remove-random test
    It creates 10 fifo transmits in total, each at a different time. The
    fifos connect to a common webcam source, read at least 10000 bytes of
    data and disconnect again.
    
    Checks whether random linking and unliking of target pipelines to the
    video source tee works without breaking the already connected streams.

--- tests/test-fifo-transmit.c
+++ tests/test-fifo-transmit.c
@@ -10,6 +10,9 @@
 #include <gaeguli/gaeguli.h>
 #include <gaeguli/fifo-transmit.h>
 #include <glib/gstdio.h>
+#include <gst/gst.h>
+
+#define FIFO_READ_LIMIT_BYTES 10000
 
 typedef struct _TestFixture
 {
@@ -77,10 +80,156 @@ test_gaeguli_fifo_transmit_same_fifo_path ()
   g_assert_null (fifo_transmit_2);
 }
 
+typedef struct
+{
+  GaeguliFifoTransmit *transmit;
+  guint transmit_id;
+  guint target_id;
+  gboolean closing;
+} FifoTestData;
+
+typedef struct
+{
+  GMutex lock;
+  TestFixture *fixture;
+  GaeguliPipeline *pipeline;
+  FifoTestData fifos[5];
+  guint fifos_to_create;
+} AddRemoveTestData;
+
+static gboolean
+add_remove_fifo_cb (AddRemoveTestData * data)
+{
+  gint i;
+
+  g_mutex_lock (&data->lock);
+
+  /* If there's a free slot, create a new fifo transmit. */
+  for (i = 0; data->fifos_to_create && i != G_N_ELEMENTS (data->fifos); ++i) {
+    FifoTestData *fifo = &data->fifos[i];
+
+    if (fifo->transmit == NULL && !fifo->closing) {
+      g_autoptr (GError) error = NULL;
+
+      fifo->transmit = gaeguli_fifo_transmit_new ();
+      g_assert_nonnull (fifo->transmit);
+
+      fifo->transmit_id = gaeguli_fifo_transmit_start (fifo->transmit,
+          "127.0.0.1", 8888, GAEGULI_SRT_MODE_CALLER, &error);
+      g_assert_no_error (error);
+
+      fifo->target_id = gaeguli_pipeline_add_fifo_target_full (data->pipeline,
+          GAEGULI_VIDEO_CODEC_H264, GAEGULI_VIDEO_RESOLUTION_640x480,
+          gaeguli_fifo_transmit_get_fifo (fifo->transmit), &error);
+      g_assert_no_error (error);
+
+      g_debug ("Added fifo %u", fifo->transmit_id);
+
+      --data->fifos_to_create;
+      break;
+    }
+  }
+
+  /* Check that all fifos are in NORMAL state and data are being read. */
+  for (i = 0; i != G_N_ELEMENTS (data->fifos); ++i) {
+    FifoTestData *fifo = &data->fifos[i];
+    GaeguliFifoTransmitStats *stats;
+    g_autoptr (GError) error = NULL;
+
+    if (fifo->transmit == NULL || fifo->closing) {
+      continue;
+    }
+
+    g_assert_cmpint (gaeguli_fifo_transmit_get_read_status (fifo->transmit), ==,
+        G_IO_STATUS_NORMAL);
+
+    stats = gaeguli_fifo_transmit_get_stats (fifo->transmit);
+    g_assert_nonnull (stats);
+
+    g_debug ("Fifo %u has read %lu B", fifo->transmit_id, stats->bytes_read);
+
+    /* Remove fifos that have read at least FIFO_READ_LIMIT_BYTES. */
+    if (stats->bytes_read >= FIFO_READ_LIMIT_BYTES) {
+      /* First stop the pipeline. */
+      gaeguli_pipeline_remove_target (data->pipeline, fifo->target_id, &error);
+      g_assert_no_error (error);
+
+      fifo->closing = TRUE;
+      /* The removal gets finished in stream_stopped_cb(). */
+    }
+  }
+
+  g_mutex_unlock (&data->lock);
+
+  return G_SOURCE_CONTINUE;
+}
+
+static void
+stream_stopped_cb (GaeguliPipeline * pipeline, guint target_id,
+    AddRemoveTestData * data)
+{
+  gint i;
+  gboolean have_active_fifos = FALSE;
+
+  g_mutex_lock (&data->lock);
+
+  for (i = 0; i != G_N_ELEMENTS (data->fifos); ++i) {
+    FifoTestData *fifo = &data->fifos[i];
+    g_autoptr (GError) error = NULL;
+
+    if (fifo->target_id == target_id) {
+      g_assert_true (fifo->closing);
+      gaeguli_fifo_transmit_stop (fifo->transmit, fifo->transmit_id, &error);
+      g_assert_no_error (error);
+      g_clear_object (&fifo->transmit);
+      fifo->closing = FALSE;
+
+      g_debug ("Removed fifo %u", fifo->target_id);
+    } else if (fifo->transmit != NULL) {
+      have_active_fifos = TRUE;
+    }
+  }
+
+  if (!have_active_fifos && data->fifos_to_create == 0) {
+    /* All fifos finished receiving, quit the test. */
+    g_main_loop_quit (data->fixture->loop);
+  }
+
+  g_mutex_unlock (&data->lock);
+}
+
+static void
+test_gaeguli_fifo_transmit_add_remove_random (TestFixture * fixture,
+    gconstpointer unused)
+{
+  AddRemoveTestData data = { 0 };
+  gint i;
+
+  g_mutex_init (&data.lock);
+  data.fixture = fixture;
+  data.pipeline = gaeguli_pipeline_new ();
+  data.fifos_to_create = 10;
+
+  g_signal_connect (data.pipeline, "stream-stopped",
+      (GCallback) stream_stopped_cb, &data);
+
+  g_timeout_add (20, (GSourceFunc) add_remove_fifo_cb, &data);
+  g_main_loop_run (fixture->loop);
+
+  g_clear_object (&data.pipeline);
+  for (i = 0; i != G_N_ELEMENTS (data.fifos); ++i) {
+    g_clear_object (&data.fifos[i].transmit);
+  }
+}
+
 int
 main (int argc, char *argv[])
 {
   g_test_init (&argc, &argv, NULL);
+  /* Don't treat warnings as fatal, which is GTest default. */
+  g_log_set_always_fatal (G_LOG_FATAL_MASK | G_LOG_LEVEL_CRITICAL);
+
+  gst_init (&argc, &argv);
 
   g_test_add_func ("/gaeguli/fifo-transmit-instance",
       test_gaeguli_fifo_transmit_instance);
@@ -91,6 +240,10 @@ main (int argc, char *argv[])
 
   g_test_add_func ("/gaeguli/fifo-transmit-same-fifo-path",
       test_gaeguli_fifo_transmit_same_fifo_path);
+
+  g_test_add ("/gaeguli/fifo-transmit-add-remove-random",
+      TestFixture, NULL, fixture_setup,
+      test_gaeguli_fifo_transmit_add_remove_random, fixture_teardown);
 
   return g_test_run ();
 }
