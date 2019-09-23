b68c15c4f56fc7a79f85b387b1e34f8f53fc6aa5
Jakub Adam <jakub.adam@collabora.com>
2019-09-23 17:25:41 +0200

    Add fifo-transmit-reattach-stream test
    Checks that a fifo is kept open after one pipeline finished streaming
    and is ready to receive data from another one.

--- tests/test-fifo-transmit.c
+++ tests/test-fifo-transmit.c
@@ -230,6 +230,97 @@ test_gaeguli_fifo_transmit_add_remove_random (TestFixture * fixture,
   }
 }
 
+static void
+reattach_stream_stream_stopped_cb (GaeguliPipeline * pipeline, guint target_id,
+    gboolean * flag)
+{
+  *flag = TRUE;
+}
+
+static gsize
+get_bytes_read (GaeguliFifoTransmit * transmit)
+{
+  g_autoptr (GVariantDict) stats = NULL;
+  g_autoptr (GVariant) bytes_read = NULL;
+
+  stats = gaeguli_fifo_transmit_get_stats (transmit);
+  g_assert_nonnull (stats);
+  bytes_read = g_variant_dict_lookup_value (stats, "bytes-read",
+      G_VARIANT_TYPE_UINT64);
+  g_assert_nonnull (bytes_read);
+
+  return g_variant_get_uint64 (bytes_read);
+}
+
+static gsize
+read_from_pipeline (GaeguliFifoTransmit * transmit, gsize bytes_read_limit)
+{
+  g_autoptr (GaeguliPipeline) pipeline = NULL;
+  g_autoptr (GError) error = NULL;
+  gboolean stream_stopped = FALSE;
+  guint target_id;
+  gsize bytes_read;
+
+  /* Set up a pipeline. */
+  pipeline = gaeguli_pipeline_new ();
+  target_id = gaeguli_pipeline_add_fifo_target_full (pipeline,
+      GAEGULI_VIDEO_CODEC_H264, GAEGULI_VIDEO_RESOLUTION_640x480,
+      gaeguli_fifo_transmit_get_fifo (transmit), &error);
+  g_assert_no_error (error);
+
+  g_debug ("Started reading from pipeline %p", pipeline);
+
+  /* Read from the fifo until we reach the desired limit. */
+  do {
+    g_main_context_iteration (g_main_context_get_thread_default (), FALSE);
+
+    bytes_read = get_bytes_read (transmit);
+    g_debug ("Fifo has read %lu B", bytes_read);
+  } while (bytes_read < bytes_read_limit);
+
+  /* Detach the pipeline from our fifo transmit. */
+  gaeguli_pipeline_remove_target (pipeline, target_id, &error);
+  g_assert_no_error (error);
+
+  /* Wait until the pipeline stops. */
+  g_signal_connect (pipeline, "stream-stopped",
+      (GCallback) reattach_stream_stream_stopped_cb, &stream_stopped);
+  while (!stream_stopped) {
+    g_main_context_iteration (g_main_context_get_thread_default (), FALSE);
+    g_debug ("Status: %d", gaeguli_fifo_transmit_get_read_status (transmit));
+  }
+
+  gaeguli_pipeline_stop (pipeline);
+
+  /* Read any data remaining in the pipe before attaching the next pipeline. */
+  while (gaeguli_fifo_transmit_get_available_bytes (transmit) > 0) {
+    g_main_context_iteration (g_main_context_get_thread_default (), FALSE);
+  }
+
+  return get_bytes_read (transmit);
+}
+
+static void
+test_gaeguli_fifo_transmit_reattach_stream (TestFixture * fixture,
+    gconstpointer unused)
+{
+  g_autoptr (GaeguliFifoTransmit) transmit = gaeguli_fifo_transmit_new ();
+  g_autoptr (GError) error = NULL;
+  gssize bytes_read;
+
+  g_main_context_push_thread_default (g_main_loop_get_context (fixture->loop));
+
+  gaeguli_fifo_transmit_start (transmit, "127.0.0.1", 8888,
+      GAEGULI_SRT_MODE_CALLER, &error);
+  g_assert_no_error (error);
+
+  /* Let two different pipelines write consecutively into one fifo. */
+  bytes_read = read_from_pipeline (transmit, FIFO_READ_LIMIT_BYTES);
+  read_from_pipeline (transmit, bytes_read + FIFO_READ_LIMIT_BYTES);
+
+  g_main_context_pop_thread_default (g_main_loop_get_context (fixture->loop));
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -252,6 +343,10 @@ main (int argc, char *argv[])
   g_test_add ("/gaeguli/fifo-transmit-add-remove-random",
       TestFixture, NULL, fixture_setup,
       test_gaeguli_fifo_transmit_add_remove_random, fixture_teardown);
+
+  g_test_add ("/gaeguli/fifo-transmit-reattach-stream",
+      TestFixture, NULL, fixture_setup,
+      test_gaeguli_fifo_transmit_reattach_stream, fixture_teardown);
 
   return g_test_run ();
 }
