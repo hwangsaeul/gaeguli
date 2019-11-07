6fa9dd2f89f8a6d42c31bb2c88245eb6b2ebf0c9
J. Kim <jeongseok.kim@sk.com>
2019-11-07 20:43:46 +0900

    tests: add test for debugging hangups on NVIDIA TX1

--- tests/test-pipeline.c
+++ tests/test-pipeline.c
@@ -27,6 +27,7 @@
 typedef struct _TestFixture
 {
   GMainLoop *loop;
+  GaeguliPipeline *pipeline;
   guint target_id;
 } TestFixture;
 
@@ -89,6 +90,60 @@ test_gaeguli_pipeline_instance (TestFixture * fixture, gconstpointer unused)
   fixture->target_id = target_id;
 
   g_main_loop_run (fixture->loop);
+
+  gaeguli_pipeline_stop (pipeline);
+}
+
+static gboolean
+_stop_pipeline (TestFixture * fixture)
+{
+  gaeguli_pipeline_stop (fixture->pipeline);
+  g_main_loop_quit (fixture->loop);
+  return G_SOURCE_REMOVE;
+}
+
+static void
+_schedule_pipeline_stop (GaeguliPipeline * pipeline, guint target_id,
+    TestFixture * fixture)
+{
+  g_timeout_add (100, (GSourceFunc) _stop_pipeline, fixture);
+}
+
+static void
+do_pipeline_cycle (TestFixture * fixture, GaeguliEncodingMethod encoding_method)
+{
+  g_autoptr (GaeguliPipeline) pipeline =
+      gaeguli_pipeline_new_full (GAEGULI_VIDEO_SOURCE_VIDEOTESTSRC, NULL,
+      encoding_method);
+  g_autoptr (GError) error = NULL;
+
+  gaeguli_pipeline_add_fifo_target_full (pipeline,
+      GAEGULI_VIDEO_CODEC_H264, GAEGULI_VIDEO_RESOLUTION_640X480,
+      "/dev/null", &error);
+
+  fixture->pipeline = pipeline;
+
+  g_signal_connect (pipeline, "stream-started",
+      G_CALLBACK (_schedule_pipeline_stop), fixture);
+
+  g_main_loop_run (fixture->loop);
+}
+
+static void
+test_gaeguli_pipeline_debug_tx1 (TestFixture * fixture, gconstpointer unused)
+{
+  GaeguliEncodingMethod encoding_method = GAEGULI_ENCODING_METHOD_GENERAL;
+  g_autoptr (GstPluginFeature) feature = NULL;
+  guint i;
+
+  feature = gst_registry_lookup_feature (gst_registry_get (), "nvvidconv");
+  if (feature) {
+    encoding_method = GAEGULI_ENCODING_METHOD_NVIDIA_TX1;
+  }
+
+  for (i = 0; i != 3; ++i) {
+    do_pipeline_cycle (fixture, encoding_method);
+  }
 }
 
 int
@@ -99,6 +154,9 @@ main (int argc, char *argv[])
   g_test_init (&argc, &argv, NULL);
   g_test_add ("/gaeguli/pipeline-instance", TestFixture, NULL, fixture_setup,
       test_gaeguli_pipeline_instance, fixture_teardown);
+
+  g_test_add ("/gaeguli/pipeline-debug-tx1", TestFixture, NULL, fixture_setup,
+      test_gaeguli_pipeline_debug_tx1, fixture_teardown);
 
   return g_test_run ();
 }
