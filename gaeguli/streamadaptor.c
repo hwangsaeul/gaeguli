cd93dbf6df4a559c884b5257be001f3189458b30
Jakub Adam <jakub.adam@collabora.com>
2020-08-13 19:04:01 +0200

    adaptor: periodically trigger SRT stats collection

--- gaeguli/streamadaptor.c
+++ gaeguli/streamadaptor.c
@@ -20,10 +20,12 @@
 
 #include <gst/gstelement.h>
 
+#define ADAPTOR_STATS_COLLECTION_INTERVAL_SECONDS 5
+
 typedef struct
 {
   GstElement *srtsink;
-
+  guint stats_timeout_id;
 } GaeguliStreamAdaptorPrivate;
 
 /* *INDENT-OFF* */
@@ -38,6 +40,47 @@ enum
 };
 
 static void
+gaeguli_stream_adaptor_collect_stats (GaeguliStreamAdaptor * self)
+{
+  GaeguliStreamAdaptorPrivate *priv =
+      gaeguli_stream_adaptor_get_instance_private (self);
+  GaeguliStreamAdaptorClass *klass = GAEGULI_STREAM_ADAPTOR_GET_CLASS (self);
+
+  g_autoptr (GstStructure) s = NULL;
+
+  g_object_get (priv->srtsink, "stats", &s, NULL);
+
+  if (gst_structure_n_fields (s) != 0) {
+    klass->on_stats (self, s);
+  }
+}
+
+gboolean
+_stats_collection_timeout (gpointer user_data)
+{
+  gaeguli_stream_adaptor_collect_stats (user_data);
+
+  return G_SOURCE_CONTINUE;
+}
+
+static void
+gaeguli_stream_adaptor_set_srtsink (GaeguliStreamAdaptor * self,
+    GstElement * srtsink)
+{
+  GaeguliStreamAdaptorPrivate *priv =
+      gaeguli_stream_adaptor_get_instance_private (self);
+  GaeguliStreamAdaptorClass *klass = GAEGULI_STREAM_ADAPTOR_GET_CLASS (self);
+
+  priv->srtsink = gst_object_ref (srtsink);
+
+  if (klass->on_stats) {
+    priv->stats_timeout_id =
+        g_timeout_add_seconds (ADAPTOR_STATS_COLLECTION_INTERVAL_SECONDS,
+        _stats_collection_timeout, self);
+  }
+}
+
+static void
 gaeguli_stream_adaptor_init (GaeguliStreamAdaptor * self)
 {
 }
@@ -47,12 +90,10 @@ gaeguli_stream_adaptor_set_property (GObject * object, guint property_id,
     const GValue * value, GParamSpec * pspec)
 {
   GaeguliStreamAdaptor *self = GAEGULI_STREAM_ADAPTOR (object);
-  GaeguliStreamAdaptorPrivate *priv =
-      gaeguli_stream_adaptor_get_instance_private (self);
 
   switch (property_id) {
     case PROP_SRTSINK:
-      priv->srtsink = g_value_dup_object (value);
+      gaeguli_stream_adaptor_set_srtsink (self, g_value_get_object (value));
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -67,6 +108,7 @@ gaeguli_stream_adaptor_dispose (GObject * object)
       gaeguli_stream_adaptor_get_instance_private (self);
 
   gst_clear_object (&priv->srtsink);
+  g_clear_handle_id (&priv->stats_timeout_id, g_source_remove);
 
   G_OBJECT_CLASS (gaeguli_stream_adaptor_parent_class)->dispose (object);
 }
