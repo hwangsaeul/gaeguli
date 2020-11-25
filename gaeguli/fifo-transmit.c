a5ee068b3f55d73685e4bac9e6b01a65dd611744
J. Kim <jeongseok.kim@sk.com>
2020-11-25 09:13:09 +0000

    fifo-transmit: do not set LINGER with wrong type

--- gaeguli/fifo-transmit.c
+++ gaeguli/fifo-transmit.c
@@ -116,7 +116,6 @@ static struct srt_constant_params srt_params[] = {
   {"SRTO_SENDER", SRTO_SENDER, 1},      /* 1: sender */
   {"SRTO_SNDSYN", SRTO_SNDSYN, 0},      /* 0: non-blocking */
   {"SRTO_RCVSYN", SRTO_RCVSYN, 0},      /* 0: non-blocking */
-  {"SRTO_LINGER", SRTO_LINGER, 0},
   {"SRTO_TSBPMODE", SRTO_TSBPDMODE, 1}, /* Timestamp-based Packet Delivery mode must be enabled */
   {"SRTO_RENDEZVOUS", SRTO_RENDEZVOUS, 0},      /* 0: not for rendezvous */
   {"SRTO_RENDEZVOUS", SRTO_RENDEZVOUS, 0},      /* 0: not for rendezvous */
